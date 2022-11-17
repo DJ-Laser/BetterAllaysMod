1ff7d74ea8ebd94efae96fdad098a24c83112848
DJ_Laser <devin.t.myers@gmail.com>
2022-11-17 16:31:47 -0500

    add stuff and block adtagen

--- src/main/java/com/djlaser/betterallays/datagen/ModBlockStateProvider.java
+++ src/main/java/com/djlaser/betterallays/datagen/ModBlockStateProvider.java
@@ -1,5 +1,20 @@
 package com.djlaser.betterallays.datagen;
 
-public class ModBlockStateProvider {
+import com.djlaser.betterallays.BetterAllays;
+import com.djlaser.betterallays.block.ModBlocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
 
+public class ModBlockStateProvider extends BlockStateProvider {
+
+    public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
+        super(gen, BetterAllays.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        simpleBlock(ModBlocks.JADE_BLOCK.get());
+        simpleBlock(ModBlocks.BUDDING_JADE.get());
+    }
 }
